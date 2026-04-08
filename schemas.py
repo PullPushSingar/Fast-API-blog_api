fd6668c03e09083603bdc39521ed7cbcf58b4d28
PullPushSingar <259550@student.pwr.edu.pl>
2026-04-08 15:06:51 +0200

    (Part 6): Completing CRUD - Update and Delete (PUT, PATCH, DELETE)

--- schemas.py
+++ schemas.py
@@ -19,6 +19,10 @@ class UserResponse(UserBase):
     image_file: str | None
     image_path: str
 
+class UserUpdate(UserBase):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(efault=None, max_length=120)
+    image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -27,6 +31,10 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     user_id: int  # TEMPORARY
+
+class PostUpdate(PostBase):
+    title: str | None = Field(default=None, min_length=1,max_length=100)
+    content: str | None = Field(default=None, min_length=1)
 
 
 class PostResponse(PostBase):
